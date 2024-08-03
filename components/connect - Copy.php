f9f7bfe1e4dfebae9f46bec67b869ae1a78ee955
Gregorius Marcellinus <marcellongkosianbhadra@gmail.com>
2024-08-03 18:39:37 +0700

    Initial Commit

---
+++ components/connect - Copy.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'mysql:host=sql207.infinityfree.com;dbname=if0_34703645_food';
+$user_name = 'if0_34703645';
+$user_password = '@AsD778899@';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Email</h3>
+         <a>lolwkwk717@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Operasional</h3>
+         <p>24 Jam</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Alamat</h3>
+         <a href="#">ITS Surabaya, Indonesia</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Telp</h3>
+         <a>081333131969</a>
+      </div>
+
+   </section>
+
+   <div class="credit"><span>mr. web designer</span></div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
