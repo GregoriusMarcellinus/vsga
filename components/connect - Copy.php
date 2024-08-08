07ce0fe066a3dcd70bb22457a0f7da0259a265e3
Gregorius Marcellinus <marcellongkosianbhadra@gmail.com>
2024-08-08 14:52:05 +0700

    Deployment

--- components/connect - Copy.php
+++ components/connect - Copy.php
@@ -2,7 +2,7 @@
 
 $db_name = 'mysql:host=localhost;dbname=vsgafood';
 $user_name = 'root';
-$user_password = '';
+$user_password = 'adawdaw'; 
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_name = 'mysql:host=localhost;dbname=vsgafood';
-$user_name = 'root';
-$user_password = '';
+$db_name = 'mysql:host=localhost;dbname=u571243125_vsgafood';
+$user_name = 'u571243125_vsgafood';
+$user_password = 'z9&O>KNHP:E'; 
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
